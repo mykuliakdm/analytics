87b9edd4f47e073e8c38ff79421f1324845076fd
Artem <artem.matseruk@gmail.com>
2024-11-29 16:43:09 +0200

    feat: updated responsive

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,8 +5,8 @@ import Link from 'next/link'
 export default function Projects() {
   return (
     <>
-      <div className="flex items-center justify-between">
-        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mb-8">
+      <div className="flex items-center justify-between mb-8">
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
           All my projects
         </h1>
         <Button asChild>
